9c4305d3d365124fe93cb79cd718f6263d41e8e8
flatron97 <flatron97@mteletec.com>
2022-07-06 17:46:51 +0900

    delete, check, add 화면에서

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<div id="app" class="container">
+  <h1 class="text-center">Todo App</h1>
+  <input v-model="todoText"
+         type="text" 
+         class="w-100"
+         placeholder="Type todo"
+         @keyup.enter="addTodo">
+  <hr>
+   <Todo v-for ="todo in todos"
+         :key="todo.id"
+         :todo="todo"
+         @toggle-checkbox="toggleCheckBox"
+         @click-delete="deleteTodo"/>
+{{todos}}
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Todo from '@/components/Todo.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    Todo
+  },
+   data(){
+        return{
+            todoText:'',
+            todos:[
+                {id:1, text:'buy a car',checked:false},
+                {id:2, text:'play game',checked:false},
+                ]
+        }
+    },
+    methods :{
+      addTodo(e){
+        this.todos.push({
+          id : Math.random(),
+          text : e.target.value,
+          checked : false
+        });
+        this.todoText='';
+      },
+
+      toggleCheckBox({id, checked}){
+        const index = this.todos.findIndex(todo=>{
+          return todo.id ===id;
+        });
+        this.todos[index].checked= checked;
+      },
+      deleteTodo(id){
+        // const index = this.todos.findIndex(todo=>{
+        //   return todo.id===id;
+        // });
+        // console.log(id);
+        // this.todos.splice(index, 1);
+        this.todos = this.todos.filter(todo=>todo.id!==id);
+      }
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,48 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+// eslint-disable-next-line vue/multi-word-component-names
+<template>
+  <div class="mb-2 d-flex">
+        <div>
+            <input type="checkbox" :checked="todo.checked"
+            @change="toggleCheckbox">
+        </div>
+        <span 
+            class="ml-3 flex-grow-1"
+            :class="todo.checked ? 'text-muted' :''"
+            :style="todo.checked ? 'text-decoration: line-through':''"
+        >
+            {{todo.text}}
+        </span>
+        <button 
+            class="btn btn-danger btn-sm"
+            @click="clickeDelete"
+        >Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        todo : {
+            type : Object,
+            required : true //필수
+        }
+    },
+    methods : {
+        //id랑 checked값을 부모 컴포넌트로 보내준다
+        toggleCheckbox(e){
+            this.$emit('toggle-checkbox',{
+                id : this.todo.id,
+                checked : e.target.checked
+            })
+        },
+        clickeDelete(){
+            this.$emit('click-delete', this.todo.id);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>    
